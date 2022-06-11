d82ab472a21a6079d926150c0ea12e7c7bcf2008
williamsilva <williamsilvaec@gmail.com>
2022-06-11 20:59:21 -0300

    first commit

---
+++ src/main/java/escola/Email.java
@@ -0,0 +1,14 @@
+package escola;
+
+public class Email {
+
+    private final String endereco;
+
+    public Email(String endereco) {
+        if (endereco == null || !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            throw new IllegalArgumentException("E-mail inválido!");
+        }
+
+        this.endereco = endereco;
+    }
+}
---
+++ src/main/java/escola/Indicacao.java
@@ -0,0 +1,12 @@
+package escola;
+
+public class Indicacao {
+
+    private Aluno indicado;
+    private Aluno indicante;
+
+    public Indicacao(Aluno indicado, Aluno indicante) {
+        this.indicado = indicado;
+        this.indicante = indicante;
+    }
+}
