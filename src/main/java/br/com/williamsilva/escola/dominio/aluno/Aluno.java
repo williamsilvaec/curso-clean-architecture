f377ac3bc73cb99d3280591c68acdb6f96270fd6
william <williamsilvaec@gmail.com>
2022-06-13 12:01:43 -0300

    serviço para cifrar a senha do aluno

--- src/main/java/br/com/williamsilva/escola/dominio/aluno/Aluno.java
+++ src/main/java/br/com/williamsilva/escola/dominio/aluno/Aluno.java
@@ -9,6 +9,7 @@ public class Aluno {
     private String nome;
     private Email email;
     private List<Telefone> telefones = new ArrayList<>();
+    private String senha;
 
     public Aluno(CPF cpf, String nome, Email email) {
         this.cpf = cpf;
